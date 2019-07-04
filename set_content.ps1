59f5b56f763ffe6df6f7e423dec37befcd4ac922
itawal <33900726+itawal@users.noreply.github.com>
2019-07-04 12:37:57 +0300

    Update set_content.ps1

--- set_content.ps1
+++ set_content.ps1
@@ -1,4 +1,8 @@
 # Set the home page.
+
+# Install IIS.
+dism /online /enable-feature /featurename:IIS-WebServerRole
+
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
   -Value "<html><body><h2>Welcome to Azure! My name is Itai.</h2></body></html>"
